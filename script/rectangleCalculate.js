c728648748dd13d63ccfe4bdeda19144e81cf3cb
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-05 01:14:44 +0600

    total area of rectangle shown

--- script/rectangleCalculate.js
+++ script/rectangleCalculate.js
@@ -2,10 +2,13 @@ function rectangleCalculate(){
     const rectangleWidth = document.getElementById("rectangle-width");
     const rectangleWidthText = rectangleWidth.value;
     const width = parseFloat(rectangleWidthText)
-    console.log(width)
 
     const rectangleLength = document.getElementById("rectangle-length");
     const rectangleLengthText = rectangleLength.value;
     const length = parseFloat(rectangleLengthText)
-    console.log(length)
+
+    const area = width * length ;
+
+    const rectangleArea = document.getElementById("rectangle-area")
+    rectangleArea.innerText = area;
 }
