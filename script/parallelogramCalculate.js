a5445e4868cbc4ab729b9fd5a282d5f71bd1e9a2
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-05 01:24:41 +0600

    get parallelogram area,input by using reusable function easily

--- script/parallelogramCalculate.js
+++ script/parallelogramCalculate.js
@@ -1,7 +1,10 @@
 function ParallelogramCalculate(){
     const base = getInputValue("Parallelogram-base")
     const height = getInputValue("Parallelogram-height")
-    console.log(base,height)
+    
+    const area = base * height;
+    setArea("parallelogram-area", area)
+
 }
 
 function getInputValue(inputFieldById){
@@ -9,4 +12,8 @@ function getInputValue(inputFieldById){
     const inputFieldText = inputField.value;
     const value = parseFloat(inputFieldText);
     return value;
+}
+function setArea(areaId, area){
+    const areaValue = document.getElementById(areaId);
+    areaValue.innerText = area;
 }
