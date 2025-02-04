9073e46ed45c264a8f468d140960e69cd3b1b58d
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-05 01:10:55 +0600

    calculate total triangle area

--- script/triangleCalculate.js
+++ script/triangleCalculate.js
@@ -2,11 +2,14 @@ function triangleCalculate(){
     const triangleBase = document.getElementById("triangle-base");
     const triangleBaseText = triangleBase.value;
     const base = parseFloat(triangleBaseText)
-    console.log(base)
 
     const triangleHeight = document.getElementById("triangle-height");
     const triangleHeightText = triangleHeight.value;
     const height = parseFloat(triangleHeightText)
-    console.log(height)
+
+    const area = 0.5 * base * height;
+
+    const triangleArea = document.getElementById("triangle-area");
+    triangleArea.innerText = area;
 
 }
